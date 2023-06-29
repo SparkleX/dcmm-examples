4b81e5b5506cba4ab7243640d82ce987b023fcff
Sun Xufei <xufei.sun@sap.com>
2023-06-29 13:44:50 +0800

    dev

--- dcmm-example-metadata/src/cards/ORDRService.ts
+++ dcmm-example-metadata/src/cards/ORDRService.ts
@@ -1,10 +1,11 @@
-import { BaseService } from "dcmm";
+import { BaseService, Context } from "dcmm";
 import { ORDRModel } from "../generated/model/ORDRModel.js";
+import { oORDRSql } from "../generated/sql/ORDRSql.js";
 
 export class ORDRService extends BaseService<ORDRModel> {
-
-    public async hello(): Promise<string> {
-        return "hello from ORDR";
+    public async hello(ctx: Context): Promise<ORDRModel[]> {
+        const data = await oORDRSql.findAll(ctx as any);
+        return data;
     }
 }
 
